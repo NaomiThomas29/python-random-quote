82e6cf43eb157575ea262f4499072052efb2bbfa
NaomiThomas29 <76224823+NaomiThomas29@users.noreply.github.com>
2020-12-31 21:18:24 -0500

    Additional exercises

--- get-quote.py
+++ get-quote.py
@@ -3,12 +3,28 @@ def primary():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
+ 
   quotes = f.readlines()
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  
+  #adding lines
+  file_object = open('quotes.txt', 'a')
+  
+  file_object.write('new line \n')
+  
+  file_object.close()
 
-  print(quotes[rnd])
+
+  
+  # printing 5 quotes
+  i = 1
+  while i<6:
+    rnd = random.randint(0, last)
+    #removing extra line when printing
+    print(quotes[rnd].strip("\n"))
+    i+=1
 
 if __name__== "__main__":
   primary()
